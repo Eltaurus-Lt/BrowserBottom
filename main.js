12a7b25597c1937e8ec8ba2d5dcafb9c7068dabf
Eltaurus-LT02 <Eltaurus@mail.ru>
2023-03-16 19:09:06 +0600

    still works

--- main.js
+++ main.js
@@ -1,68 +1,75 @@
-//understand execution order
-//rtc roll dice
-//player names
-
-//separate agora.js
-//async refactor + ...
+//Refactor + split | understand execution order
 //audio + controls + playerlist/leave
 
-//room+appid for a share link
-
-function logRoll(player, roll) {
-    let log = document.getElementById("log");
-    let logentry = log.appendChild(document.createElement("div"));
-    logentry.innerHTML = (player ? player : "Player ?") + " rolled " + roll;
-    log.scrollTop = log.scrollHeight;
-}
-
-function diceRoll() {
-
-    let roll = Math.floor(100 * Math.random());
-    //console.log("dice rolled");
-
-    if (peerConnection && dataChannel) {
-        dataChannel.send(JSON.stringify({
-          type: 'roll',
-          value: roll
-        }));
-    }
-   
-    logRoll("I", roll);
-}
+//player colors
+//test room invite link
+//?reenter as new/old user
 
 
+//Game Setting
+let CurrentPlayer = localStorage.getItem('BB-Name');
 
-let APP_ID = localStorage.getItem('BB-AppID');
 let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 let roomId = urlParams.get('room');
-if (!roomId || !APP_ID) {
+if (roomId && roomId!="")  {
+    localStorage.setItem('BB-Room', roomId);
+}
+if (!roomId || !CurrentPlayer) {
+    window.location = "index.html";
+}
+
+function callEvent(event, data) {
+    document.dispatchEvent(
+        new CustomEvent(event, {detail: data})
+    );
+}
+
+//AGORA login
+let APP_ID = localStorage.getItem('BB-AppID');
+
+if (!APP_ID) {
     window.location = "index.html";
 }
 
 let token = null;
-let uid = String(Math.floor(Math.random() * 1000));
+let uid = CurrentPlayer;
 let client;
 let channel; 
 
-let localStream;
-let remoteStream;
-let peerConnection;
-let dataChannel;
+async function init() {
+    client = await AgoraRTM.createInstance(APP_ID);
+    await client.login({uid, token});
+    channel = client.createChannel(roomId);
+    await channel.join();
 
-const servers = {
-    iceServers:[
-        {
-            urls:["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"]
-        }
-    ]
+    channel.on('MemberJoined', handleUserJoined);
+    channel.on('MemberLeft', handleUserLeft);
+    client.on('MessageFromPeer', handleAgoraMessage);
+};
+init();
+
+async function leaveChannel() {
+    await channel.leave();
+    await client.logout();
+}
+window.addEventListener('beforeunload', leaveChannel);
+
+
+async function handleUserJoined(PlayerId) {
+    console.log(PlayerId, "joined the party \\o/");
+    createOffer(PlayerId);
 }
 
-let handleMessageFromPeer = async (message, MemberId) => {
+function handleUserLeft(PlayerId) {
+
+}
+
+async function handleAgoraMessage(message, PlayerId) {
     message = JSON.parse(message.text);
 
     if (message.type === 'offer') {
-        createAnswer(MemberId, message.offer);
+        createAnswer(PlayerId, message.offer);
     }
 
     if (message.type === 'answer') {
@@ -77,104 +84,146 @@ let handleMessageFromPeer = async (message, MemberId) => {
 
 }
 
-let handleUserJoined = async (MemberId) => {
-    console.log('new player joined: ', MemberId);
-    createOffer(MemberId);
-}
 
-let handleUserLeft = (MemberId) => {
+//Agora2RTC
+const servers = {iceServers:[{ urls:["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"]}]};
 
-}
+let peerConnection;
+let localStream;
+let remoteStream;
+let dataChannel;
 
-let init = async () => {
-    client = await AgoraRTM.createInstance(APP_ID);
-    await client.login({uid, token});
-    channel = client.createChannel(roomId);
-    await channel.join();
-
-    channel.on('MemberJoined', handleUserJoined);
-    channel.on('MemberLeft', handleUserLeft);
-    client.on('MessageFromPeer', handleMessageFromPeer);
-
-
-//    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio: false});
-//   document.getElementById('video1').srcObject = localStream;
-}
-
-let createPeerConnection = async (MemberId) => {
-    peerConnection = new RTCPeerConnection(servers);
-
+async function createMediaStreams() {
     remoteStream = new MediaStream();
     document.getElementById('video2').srcObject = remoteStream;
 
- //   if (!localStream) {
-        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio: false});
-        document.getElementById('video1').srcObject = localStream;
- //   }
-
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
     localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream);
     });
+}
+
+async function createPeerConnection(PlayerId) {
+    peerConnection = new RTCPeerConnection(servers);
+
+    await createMediaStreams();
+
 
     peerConnection.ontrack = (event) => {
         event.streams[0].getTracks().forEach((track) => {
             remoteStream.addTrack(track);
-        })
-    }
+        });
+    };
 
     peerConnection.onicecandidate = async (event) => {
         if (event.candidate) {
             //console.log("New ICE candidate:", event.candidate);
-            client.sendMessageToPeer({text: JSON.stringify({'type': "candidate", 'candidate': event.candidate})}, MemberId);
+            client.sendMessageToPeer({ text: JSON.stringify({ 'type': "candidate", 'candidate': event.candidate }) }, PlayerId);
         }
-    }
+    };
 
     //data
     dataChannel = peerConnection.createDataChannel('GameData');
- 
+
     peerConnection.ondatachannel = async (event) => {
         const dataChannel = event.channel;
-      
+
         dataChannel.onmessage = event => {
-          const data = JSON.parse(event.data);
-      
-          if (data.type === 'roll') {
-            logRoll(null, data.value);
-          }
+            const data = JSON.parse(event.data);
+
+            if (data.type === 'roll') {
+                logRoll(data.player, data.value);
+            }
         };
     };
 }
 
-let createOffer = async (MemberId) => {
-    await createPeerConnection(MemberId);
+async function createOffer(PlayerId) {
+    await createPeerConnection(PlayerId);
 
     let offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
 
-    client.sendMessageToPeer({text: JSON.stringify({'type': "offer", 'offer': offer})}, MemberId);
+    client.sendMessageToPeer({ text: JSON.stringify({ 'type': "offer", 'offer': offer }) }, PlayerId);
 }
 
-let createAnswer = async (MemberId, offer) => {
-    await createPeerConnection(MemberId);
+
+
+//RTC-Client
+function sendGameState() {
+
+}
+
+function sendGameData(type, value) {
+
+    if (peerConnection && dataChannel) {
+        dataChannel.send(JSON.stringify({
+          type: type,
+          value: value,
+          player: CurrentPlayer
+        }));
+    } else {
+        console.log("Connection error: no peerConnection or dataChannel");
+    }
+}
+
+function receiveGameData(data) {
+
+}
+
+
+//Game
+function logRoll(player, roll) {
+    let log = document.getElementById("log");
+    let logentry = log.appendChild(document.createElement("div"));
+    logentry.innerHTML = (player ? player : "Player ?") + " rolled " + roll;
+    log.scrollTop = log.scrollHeight;
+}
+
+function diceRoll() {
+    let roll = Math.floor(100 * Math.random());
+    sendGameData("roll", roll);  
+    logRoll(CurrentPlayer, roll);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function createAnswer(PlayerId, offer) {
+    await createPeerConnection(PlayerId);
 
     await peerConnection.setRemoteDescription(offer);
 
     let answer = await peerConnection.createAnswer();
     await peerConnection.setLocalDescription(answer);
 
-    client.sendMessageToPeer({text: JSON.stringify({'type': "answer", 'answer': answer})}, MemberId);
+    client.sendMessageToPeer({ text: JSON.stringify({ 'type': "answer", 'answer': answer }) }, PlayerId);
 }
 
-let addAnswer = async (answer) => {
+async function addAnswer(answer) {
     if (!peerConnection.currentRemoteDescription) {
         peerConnection.setRemoteDescription(answer);
     }
 }
 
-let leaveChannel = async () => {
-    await channel.leave();
-    await client.logout();
-}
-window.addEventListener('beforeunload', leaveChannel);
 
-init();
+
+
