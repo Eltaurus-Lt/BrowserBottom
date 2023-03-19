8f5857d09417d48b8f0a6e62b9f6f5df140533fc
Eltaurus-LT02 <Eltaurus@mail.ru>
2023-03-19 06:26:55 +0600

    extract Agora module + scripts folder

---
+++ scripts/Agora.js
@@ -0,0 +1,75 @@
+//AGORA login
+/*
+requires:
+    CurrentPlayer
+    roomId
+
+defines:
+    signalToPeer(data, PlayerId)
+    initConnection()
+
+triggers:
+    playerJoined data.{PlayerId}
+    playerLeft data.{PlayerId}
+    createAnswer.{offer, toPlayer}
+    addAnswer.{answer, fromPlayer}
+    icecandidate.{candidate}
+    [receiveGameData.{...}]
+
+    leave+logout before unload
+*/
+let APP_ID = localStorage.getItem('BB-AppID');
+if (!APP_ID) {
+    window.location = "index.html";
+}
+let token = null;
+let uid = CurrentPlayer;
+let client;
+let channel;
+
+async function initConnection() {
+    client = await AgoraRTM.createInstance(APP_ID);
+    await client.login({ uid, token });
+    channel = client.createChannel(roomId);
+    await channel.join();
+
+    channel.on('MemberJoined', AgoraUserJoined);
+    channel.on('MemberLeft', AgoraUserLeft);
+    client.on('MessageFromPeer', AgoraMessage);
+
+}
+;
+
+async function AgoraUserJoined(PlayerId) {
+    triggerEvent('playerJoined', { PlayerId: PlayerId });
+}
+function AgoraUserLeft(PlayerId) {
+    triggerEvent('playerLeft', { PlayerId: PlayerId });
+}
+function signalToPeer(data, to) {
+    client.sendMessageToPeer({ text: JSON.stringify(data) }, to);
+}
+
+async function AgoraMessage(message, fromPlayer) {
+    triggerEvent('signalFromPeer', Object.assign({}, JSON.parse(message.text), { from: fromPlayer }));
+}
+window.addEventListener('beforeunload', async () => { await channel.leave(); await client.logout(); });
+//RTCsignaling + messages
+catchEvent('signalFromPeer', data => {
+    if (data.type === 'offer') {
+        triggerEvent('createAnswer', { 'offer': data.content, 'toPlayer': data.from });
+    }
+
+    if (data.type === 'answer') {
+        triggerEvent('addAnswer', { 'answer': data.content, 'fromPlayer': data.from });
+    }
+
+    if (data.type === 'candidate') {
+        triggerEvent('icecandidate', { 'candidate': data.content });
+    }
+
+    if (data.type === 'message') { //untested
+        triggerEvent('receiveGameData', data.content);
+    }
+    //convert to switch 
+});
